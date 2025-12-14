4ae36b5adcc71c2133cfd998a7264a3d7409e76b
Kholin <oodzchen@gmail.com>
2025-12-14 10:30:11 +0800

    feat: add redis permissions

--- internal/services/generator.go
+++ internal/services/generator.go
@@ -695,7 +695,8 @@ services:
       sh -c '
         cp /src/redis.conf /config/
         echo "user default +@all ~* on >$$REDISCLI_AUTH" > /config/users.acl
-        echo "user $$REDIS_USER +@read +@write +@list +@hash +@set +@string +@connection +scan +del +exists +type +ttl +expire ~* -@admin -@dangerous on >$$REDIS_PASSWORD" >> /config/users.acl
+        echo "user $$REDIS_USER +@read +@write +@list +@hash +@set +@string +@connection +@scripting +scan +del +exists +type +ttl +expire ~* -@admin -@dangerous on >$$REDIS_PASSWORD" >> /config/users.acl
+        echo "Rate limiter keys will use prefix: global_ip_*, role_*, site_*" >> /config/users.acl
         echo "ACL file generated successfully"
         echo "Users configured: default (admin access), $$REDIS_USER (app access)"
         echo "ACL file location: /config/users.acl"
--- templates/redis/users.acl.tpl
+++ templates/redis/users.acl.tpl
@@ -1,2 +1,2 @@
 user default +@all ~* on >$REDISCLI_AUTH
-user $REDIS_USER +@read +@write +@list +@hash +@set +@string +@bitmap +@hyperloglog +@geo +@stream +@connection -@admin -@dangerous on >$REDIS_PASSWORD
+user $REDIS_USER +@read +@write +@list +@hash +@set +@string +@bitmap +@hyperloglog +@geo +@stream +@connection +@scripting -@admin -@dangerous on >$REDIS_PASSWORD
